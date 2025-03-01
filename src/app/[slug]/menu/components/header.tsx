6e776afa60d3e286408ab58ea4172aa2b1b7d138
Rian Xavier <rianalvesxvr@gmail.com>
2025-03-01 13:02:19 -0300

    feat: finished order-list page and cart page

--- src/app/[slug]/menu/components/header.tsx
+++ src/app/[slug]/menu/components/header.tsx
@@ -1,31 +1,47 @@
 "use client";
 
 import { Restaurant } from "@prisma/client";
-import { Pick } from "@prisma/client/runtime/library";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
-import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { useParams, useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 
-interface RestauranHeaderProps {
-    restaurant: Pick<Restaurant, 'name' | 'coverImageUrl'>
+interface RestaurantHeaderProps {
+    restaurant: Pick<Restaurant, "name" | "coverImageUrl">;
 }
 
-const RestaurantHeader = ({ }: RestauranHeaderProps) => {
+const RestaurantHeader = ({ restaurant }: RestaurantHeaderProps) => {
+    const { slug } = useParams<{ slug: string }>();
     const router = useRouter();
-
+    const handleBackClick = () => router.back();
+    const handleOrdersClick = () => router.push(`/${slug}/orders`);
     return (
-        <div className="relative h-[250px] w-full bg-slate-500">
-            <Button variant="secondary" size="icon" className="absolute left-4 top-4 z-50 rounded-full"
-                onClick={() => router.back()}>
+        <div className="relative h-[250px] w-full">
+            <Button
+                variant="secondary"
+                size="icon"
+                className="absolute left-4 top-4 z-50 rounded-full"
+                onClick={handleBackClick}
+            >
                 <ChevronLeftIcon />
             </Button>
-            {/* <Image src={restaurant.coverImageUrl} fill alt={restaurant.name} className="object-cover" /> */}
-            <Button variant="secondary" size="icon" className="absolute right-4 top-4 z-50 rounded-full">
+            <Image
+                src={restaurant.coverImageUrl}
+                alt={restaurant.name}
+                fill
+                className="object-cover"
+            />
+            <Button
+                variant="secondary"
+                size="icon"
+                className="absolute right-4 top-4 z-50 rounded-full"
+                onClick={handleOrdersClick}
+            >
                 <ScrollTextIcon />
             </Button>
         </div>
     );
-}
+};
 
 export default RestaurantHeader;
--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -3,27 +3,45 @@
 import { Product } from "@prisma/client";
 import { createContext, ReactNode, useState } from "react";
 
-interface CartProduct extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
+export interface CartProduct extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
     quantity: number;
 }
 
 export interface ICartContext {
     isOpen: boolean;
     products: CartProduct[];
+    total: number;
+    totalQuantity: number;
     toggleCart: () => void;
     addProduct: (product: CartProduct) => void
+    decreaseProductQuantity: (productId: string) => void
+    increaseProductQuantity: (productId: string) => void
+    removeProduct: (productId: string) => void
 }
 
 export const CartContext = createContext<ICartContext>({
     isOpen: false,
     products: [],
+    total: 0,
+    totalQuantity: 0,
     toggleCart: () => { },
-    addProduct: () => { }
+    addProduct: () => { },
+    decreaseProductQuantity: () => { },
+    increaseProductQuantity: () => { },
+    removeProduct: () => { },
 })
 
 export const CartProvider = ({ children }: { children: ReactNode }) => {
     const [products, setProducts] = useState<CartProduct[]>([])
     const [isOpen, setIsOpen] = useState<boolean>(false)
+
+    const total = products.reduce((acc, product) => {
+        return acc + product.price * product.quantity;
+    }, 0)
+
+    const totalQuantity = products.reduce((acc, product) => {
+        return acc + product.quantity
+    }, 0)
 
     const toggleCart = () => {
         setIsOpen((prev) => !prev)
@@ -49,12 +67,45 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
         });
     };
 
+    const decreaseProductQuantity = (productId: string) => {
+        setProducts(prevProducts => {
+            return prevProducts.map(prevProduct => {
+                if (prevProduct.id === productId && prevProduct.quantity !== 1)
+                    return { ...prevProduct, quantity: prevProduct.quantity - 1 }
+
+                return prevProduct
+            })
+
+        })
+    }
+
+    const increaseProductQuantity = (productId: string) => {
+        setProducts(prevProducts => {
+            return prevProducts.map(prevProduct => {
+                if (prevProduct.id === productId)
+                    return { ...prevProduct, quantity: prevProduct.quantity + 1 }
+
+                return prevProduct
+            })
+
+        })
+    }
+
+    const removeProduct = (productId: string) => {
+        setProducts(prevProducts => prevProducts.filter(prevProduct => prevProduct.id !== productId))
+    }
+
     return (
         <CartContext.Provider value={{
             isOpen,
             products,
+            total,
+            totalQuantity,
             toggleCart,
-            addProduct
+            addProduct,
+            decreaseProductQuantity,
+            increaseProductQuantity,
+            removeProduct
         }}>
             {children}
         </CartContext.Provider>
